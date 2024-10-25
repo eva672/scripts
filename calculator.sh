d19e99a18002078db204abf7b1b7ea6bd3092257
eva672 <evamanuska@gmail.com>
2024-10-25 11:50:31 +0100

    calculator.sh

---
+++ calculator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# A script that create simple calculator
+echo "enter first number"
+read number1
+echo "enter second number"
+read number2
+echo "operation (+,*,-,/)"
+read operation
+result=$(echo "scale=3;$number1 $operation $number2" | bc -l)
+echo "Result; $result"
