17e898d7694304005e79d44a064e2d215c3aea74
eva672 <evamanuska@gmail.com>
2024-10-25 11:37:04 +0100

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -4,7 +4,7 @@ echo "enter first number"
 read number1
 echo "enter second number"
 read number2
-echo "operation (+,*,-,/)"
+echo "operation (+,*,-,/)" 
 read operation
 result=$(echo "scale=3;$number1 $operation $number2" | bc -l)
 echo "Result; $result"
